ff8dde548ea0c2156123c74aaeed0de0926717e0
cokeefe <chaemobile@gmail.com>
2013-09-16 19:08:56 -0700

    category migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraiglistRails::Application.config.secret_token = '5683dc4f1fe34d4d91556735115fce093524f650a4d246c435a9eeeb8f6461b74cfe18f97be9f347a2266259f02302bfe40d34063a79739b023ab5c663bf7e22'
---
+++ db/migrate/20130917020749_categories.rb
@@ -0,0 +1,10 @@
+class Categories < ActiveRecord::Migration
+
+  def change
+    create_table :categories do |t|
+      t.string :description
+      t.timestamps
+    end
+  end
+
+end
