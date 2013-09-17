aa92fd8f04a5095604a7b77e70b10bea862316f4
cokeefe <chaemobile@gmail.com>
2013-09-16 20:32:05 -0700

    new view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,10 @@ class PostsController < ApplicationController
     @category = Category.find(params[:category_id])
     @posts = @category.posts
   end
+
+  def new
+  end
+
+  def create
+  end
 end
