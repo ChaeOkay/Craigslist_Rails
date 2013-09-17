19d7c2e72992bd7e0881552c19c30e9c2e47f6eb
cokeefe <chaemobile@gmail.com>
2013-09-16 21:14:07 -0700

    create form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @category = Category.find(params[:category_id])
+    @post = Post.new
   end
 
   def create
+    @post = Post.create( {title: params[:title],
+                  description: params[:description],
+                  email: params[:email],
+                  secure_email: "fjf8932983hiufewf28y9@craigslistjr.com",
+                  rent: params[:rent],
+                  area: params[:area],
+                  key: "wzdzihu7832" } )
   end
 end
